898fcaf55da24cbfe2d8df34c5a9e4e278ded60c
Grazielly-Araujo <graziellygalvaoaraujo@gmail.com>
2022-10-18 21:43:02 -0300

    Formulario_Django

---
+++ viagens/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from viagens.forms import ViagemForms
+
+def index(request):
+    form = ViagemForms()
+    contexto = {'form': form}
+    return render( request, 'index.html', contexto)
+
+def revConsulta (request):
+    if request.method == 'POST':
+        form = ViagemForms (request.POST)
+        contexto = { 'form': form}
+        return render (request, 'consulta.html', contexto)
+
+
+
+# Create your views here.
